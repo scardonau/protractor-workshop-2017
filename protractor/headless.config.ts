c685bf97c6d67865e2b2abe0bbd6d8058a593a86
Santiago Cardona Urrea <scardonau@pslcol.com.co>
2018-06-15 07:51:51 -0500

    get page wait and jasmine waits implemented on config

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -5,7 +5,7 @@ export const config: Config = {
   framework: 'jasmine',
   SELENIUM_PROMISE_MANAGER: false,
   specs: ['../test/**/*.spec.js'],
-  getPageTimeout: 1000,
+  getPageTimeout: 30000,
   noGlobals: true,
   capabilities: {
     browserName: 'chrome',
@@ -13,6 +13,7 @@ export const config: Config = {
       args: ['--headless', '--disable-gpu', '--window-size=800,600', '--no-sandbox']
     }
   },
+  jasmineNodeOpts: { defaultTimeoutInterval: 120000 },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
