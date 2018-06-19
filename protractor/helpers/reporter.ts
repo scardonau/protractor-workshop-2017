c284e9598c97b6bd4f08197c512e361378431773
scardonau <40206329+scardonau@users.noreply.github.com>
2018-06-19 09:29:49 -0500

    added reports with jasmine awesome (#39)

--- protractor/helpers/reporter.ts
+++ protractor/helpers/reporter.ts
@@ -1,7 +1,16 @@
 import { DisplayProcessor, SpecReporter } from 'jasmine-spec-reporter';
+const { AwesomeReport } = require('jasmine-awesome-report');
 
 export let reporter = () => {
   jasmine.getEnv().addReporter(new SpecReporter({
     customProcessors: [DisplayProcessor],
   }));
+
+  const config = {
+    fullPath: 'reports',
+    fileName: 'awesome',
+    merge: true
+  };
+  
+  jasmine.getEnv().addReporter(AwesomeReport.getReport(config));
 };
