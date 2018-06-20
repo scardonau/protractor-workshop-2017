6ba9eefdbbc2a709d15fca1ac2bfb5b76f6f106a
Santiago Cardona Urrea <scardonau@pslcol.com.co>
2018-06-20 14:29:24 -0500

    javasccript code execution

---
+++ src/page/IFrame.page.ts
@@ -0,0 +1,15 @@
+import { ElementFinder, promise, element, by, browser } from 'protractor';
+
+export class IFramePage {
+  private get iFrame1(): ElementFinder {
+    return element(by.id('IF1'));
+  }
+
+  public changeIFrameHeight(newHeight): promise.Promise<void> {
+    return browser.executeScript(`document.getElementById("IF1").setAttribute('height', ${newHeight})`)
+  }
+  public async getIFrameHeight(): Promise<number> {
+    const height = await this.iFrame1.getAttribute('height');
+    return Number(height);
+  }
+}
