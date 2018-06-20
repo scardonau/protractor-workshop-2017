34c4c916b0cdd46452921f977ebf3906fd0f3262
Santiago Cardona Urrea <scardonau@pslcol.com.co>
2018-06-20 16:00:24 -0500

    wrkoing with iframes

--- src/page/IFrame.page.ts
+++ src/page/IFrame.page.ts
@@ -5,12 +5,29 @@ export class IFramePage {
     return element(by.id('IF1'));
   }
 
+  private get pageTitle(): ElementFinder {
+    return element(by.css('#content > h1'));
+  }
+
   public changeIFrameHeight(newHeight): promise.Promise<void> {
     return browser
       .executeScript(`document.getElementById("IF1").setAttribute('height', ${newHeight})`);
   }
+
   public async getIFrameHeight(): Promise<number> {
     const height = await this.iFrame1.getAttribute('height');
     return Number(height);
+  }
+
+  public changeToIFrame1(): promise.Promise<void> {
+    return browser.switchTo().frame(this.iFrame1.getWebElement());
+  }
+
+  public changeToMainpage(): promise.Promise<void> {
+    return browser.switchTo().defaultContent();
+  }
+
+  public getTitleText(): promise.Promise<string> {
+    return this.pageTitle.getText();
   }
 }
