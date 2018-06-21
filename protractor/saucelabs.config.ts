5043451939676c8301b2aa5e81f2680564254219
Santiago Cardona Urrea <scardonau@pslcol.com.co>
2018-06-21 11:26:43 -0500

    saucelabs testing implemented

---
+++ protractor/saucelabs.config.ts
@@ -0,0 +1,28 @@
+import { browser, Config } from 'protractor';
+import { reporter } from './helpers/reporter';
+
+export let config: Config = {
+  framework: 'jasmine',
+  specs: ['../test/**/*.spec.js'],
+  SELENIUM_PROMISE_MANAGER: false,
+  noGlobals: true,
+  getPageTimeout: 30000,
+  capabilities: {
+    name: 'UI Workshop',
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['disable-infobars=true --window-size=800,600'],
+      prefs: { credentials_enable_service: false }
+    }
+  },
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
+  onPrepare: () => {
+    reporter();
+    browser.ignoreSynchronization = true;
+    browser.manage().timeouts().implicitlyWait(0);
+  },
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY
+};
