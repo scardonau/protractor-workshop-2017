89d561ee44309d04e9318da41202ec5a2bd7a09a
scardonau <40206329+scardonau@users.noreply.github.com>
2018-06-22 08:59:01 -0500

    wrkoing with iframes (#112)

--- test/IFrame.spec.ts
+++ test/IFrame.spec.ts
@@ -1,5 +1,5 @@
 import { browser } from 'protractor';
-import { IFramePage } from '../src/page';
+import { IFramePage, PersonalInformationPage } from '../src/page';
 
 describe('Given that we visit a page with an Iframe', () => {
   const iframePageUrl = 'http://toolsqa.com/iframe-practice-page/';
@@ -11,7 +11,7 @@ describe('Given that we visit a page with an Iframe', () => {
 
   describe('when we try to modify the Iframe 1 height', () => {
     let newHeight;
-    const expectedHeight = 150;
+    const expectedHeight = 850;
 
     beforeAll(async () => {
       await iFramePage.changeIFrameHeight(expectedHeight);
@@ -20,6 +20,46 @@ describe('Given that we visit a page with an Iframe', () => {
 
     it(`then the new Iframe height should be ${expectedHeight}`, () => {
       expect(newHeight).toBe(expectedHeight);
+    });
+
+    describe('when get the page title', () => {
+      let pageTitle;
+      const expectedPageTitle = 'Sample Iframe page';
+
+      beforeAll(async () => {
+        pageTitle = await iFramePage.getTitleText();
+      });
+
+      it(`then it should be '${expectedPageTitle}'`, () => {
+        expect(pageTitle).toBe(expectedPageTitle);
+      });
+
+      describe('when we get into the IFrame 1', () => {
+        let iFrame1Title;
+        const expectedIFrame1Title = 'Practice Automation Form';
+        const personalInformationPage : PersonalInformationPage = new PersonalInformationPage();
+
+        beforeAll(async () => {
+          await iFramePage.changeToIFrame1();
+          iFrame1Title = await personalInformationPage.getTitleText();
+        });
+
+        it(`then the title should be '${expectedIFrame1Title}'`, () => {
+          expect(iFrame1Title).toBe(expectedIFrame1Title);
+        });
+
+        describe('when we go back to the main page', () => {
+
+          beforeAll(async () => {
+            await iFramePage.changeToMainpage();
+            pageTitle = await iFramePage.getTitleText();
+          });
+
+          it(`then the title should be ${expectedPageTitle}`, () => {
+            expect(pageTitle).toBe(expectedPageTitle);
+          });
+        });
+      });
     });
   });
 });
